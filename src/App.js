9a420720e873ac8bc8df8b729d25372923cc0de6
beringui1611 <fortiniteberingui12@gmail.com>
2023-08-23 13:31:20 -0300

    teste-wagner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import api from "./api";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function LoadProducts() {
+      try {
+        const response = await api.get(
+          "https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json"
+        );
+
+        if (response.data && response.data.success) {
+          setProducts(response.data.products);
+        } else {
+          console.error("API response format is incorrect.");
+        }
+      } catch (error) {
+        console.error("Error fetching data from API:", error);
+      }
+    }
+
+    LoadProducts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {products.map((product, index) => (
+        <div key={index}>
+          <p>{product.productName}</p>
+          <p>{product.descriptionShort}</p>
+          <img src={product.photo} alt={product.productName} />
+          <p>{product.price}</p>
+        </div>
+      ))}
     </div>
   );
 }
